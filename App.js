554d4ce5451d3660b3b083b05937ffecdc12b91b
Yeyul Choi <yeyulchoi2@gmail.com>
2025-04-11 00:01:38 -0400

    lecture-01

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+    "h1", 
+    {id:"heading",
+        color:"yellow"
+    }, // this object is used to give these attribute (h1) to a tag
+    "Hello World from React!!.")
+
+const root = ReactDOM.createRoot(document.getElementById("root"))  // root is a place where all react codes will run.
+
+root.render(heading)     //render is convering obj to html adding brace to tag
---
+++ README.md
@@ -0,0 +1,1 @@
+# PRO Yeyul React
